4056f53a43ec7103b2bf0aa2f8d7135b175f55aa
Michael Limiero <mike5713@gmail.com>
2024-09-06 10:19:00 -0400

    wip

--- boards/deltawhy/epsilon/main.py
+++ boards/deltawhy/epsilon/main.py
@@ -5,29 +5,34 @@ import board
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners.keypad import KeysScanner
+from kmk.scanners import DiodeOrientation
 from kmk.modules.layers import Layers
 from asetniop import Asetniop
 
 keyboard = KMKKeyboard()
+keyboard.col_pins = (board.D0, board.D1, board.D2, board.D3)
+keyboard.row_pins = (board.D7, board.D8, board.D9)
+keyboard.diode_orientation = DiodeOrientation.COL2ROW
 keyboard.modules.append(Layers())
 
-from dictionaries import QWERTY_COMBOS, COLEMAK_COMBOS, COLEMAK_WORDS
+from dictionaries import COLEMAK_COMBOS
 
-asetniop = Asetniop(COLEMAK_COMBOS + COLEMAK_WORDS)
+#asetniop = Asetniop(COLEMAK_COMBOS + COLEMAK_WORDS)
+asetniop = Asetniop(COLEMAK_COMBOS)
 keyboard.modules.append(asetniop)
 
-_KEY_CFG = [
-        board.A0, board.A1, board.A2, board.A3,
-        board.D2, board.D3, board.D4, board.D5,
-        board.D10, board.D9
-]
-
-keyboard.matrix = KeysScanner(
-        pins=_KEY_CFG,
-        value_when_pressed=False,
-        pull=True,
-        interval=0.02,
-        max_events=64)
+# _KEY_CFG = [
+#        board.A0, board.A1, board.A2, board.A3,
+#        board.D2, board.D3, board.D4, board.D5,
+#        board.D10, board.D9
+# ]
+#
+# keyboard.matrix = KeysScanner(
+#        pins=_KEY_CFG,
+#        value_when_pressed=False,
+#        pull=True,
+#        interval=0.02,
+#        max_events=64)
 
 # keyboard.keymap = [
 #     [KC.A, KC.S, KC.E, KC.T,    KC.N, KC.I, KC.O, KC.P,
